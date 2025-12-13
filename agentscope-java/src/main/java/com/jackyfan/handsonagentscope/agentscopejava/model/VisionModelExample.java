2f659037e04d4bf90dc053b23e82a0f81911ce26
jacky <jackyfan@163.com>
2025-12-13 18:42:28 +0800

    视频模型

---
+++ agentscope-java/src/main/java/com/jackyfan/handsonagentscope/agentscopejava/model/VisionModelExample.java
@@ -0,0 +1,51 @@
+package com.jackyfan.handsonagentscope.agentscopejava.model;
+
+import io.agentscope.core.message.*;
+import io.agentscope.core.model.ChatResponse;
+import io.agentscope.core.model.DashScopeChatModel;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.List;
+
+public class VisionModelExample {
+    public static void main(String[] args) throws IOException {
+        // 创建视觉模型
+        DashScopeChatModel visionModel = DashScopeChatModel.builder()
+                .apiKey(System.getenv("DASHSCOPE_API_KEY"))
+                .modelName("qwen-vl-max")  // 视觉模型
+                .build();
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("classpath:/pics/coder.jpeg");
+        String base64Image = Base64.getEncoder().encodeToString(
+                Files.readAllBytes(Paths.get(resource.getURI()))
+        );
+        // 准备多模态消息
+        Msg imageMsg = Msg.builder()
+                .name("user")
+                .role(MsgRole.USER)
+                .content(List.of(
+                        TextBlock.builder().text("这张图片里有什么？").build(),
+                        ImageBlock.builder().source(Base64Source.builder()
+                                .data(base64Image)
+                                .mediaType("image/jpg")
+                                .build()).build()
+                ))
+                .build();
+
+        // 生成响应
+        visionModel.stream(List.of(imageMsg), null, null).flatMapIterable(ChatResponse::getContent).map(block -> {
+            if (block instanceof TextBlock tb) return tb.getText();
+            if (block instanceof ThinkingBlock tb) return tb.getThinking();
+            if (block instanceof ToolUseBlock tub) return tub.getContent();
+            return "";
+        }).filter(text -> !text.isEmpty()).doOnNext(System.out::print).blockLast();
+        System.exit(0);
+    }
+
+}
